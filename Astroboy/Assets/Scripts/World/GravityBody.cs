341623ff231cc6c9a8de56f85a2509a58a891744
AntoinouM <92710938+AntoinouM@users.noreply.github.com>
2023-06-13 23:30:41 +0200

    init: movment player

--- Astroboy/Assets/Scripts/World/GravityBody.cs
+++ Astroboy/Assets/Scripts/World/GravityBody.cs
@@ -4,39 +4,32 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class GravityBody : MonoBehaviour
-{    
-    [SerializeField] private bool isTest;
-    [Space(10)]
+{
     [SerializeField] private GravityAttractor attractor;
     [SerializeField] [Range(0, 1)] private float smoothInterpolationParam = 1f;
-    [SerializeField] private GameObject gravitySource;
+    [SerializeField][Range(1, 1000)] private float objectMass = 500f;
     [Space(10)]
 
-    private float _distanceBetweenObjects;
     private Rigidbody _myRigidBody;
     private Transform _myTransform;
-    private MeshRenderer _gravitySourceMeshRenderer;
     
     // Start is called before the first frame update
     void Start()
     {
+        // set RigidBody parameters
         _myRigidBody = GetComponent<Rigidbody>();
         _myRigidBody.constraints = RigidbodyConstraints.FreezeRotation;
         _myRigidBody.useGravity = false;
+        _myRigidBody.mass = objectMass;
+        _myRigidBody.interpolation = RigidbodyInterpolation.Extrapolate;
+        _myRigidBody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        
         _myTransform = transform;
-        _gravitySourceMeshRenderer = gravitySource.GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _distanceBetweenObjects = Vector3.Distance(transform.position, gravitySource.transform.position) - (_gravitySourceMeshRenderer.bounds.size.z / 2);
         attractor.Attract(_myTransform, _myRigidBody, smoothInterpolationParam);
-        
-        if (isTest)
-        {
-            Debug.DrawRay(transform.position, transform.up * 10, Color.magenta);
-            Debug.Log(_distanceBetweenObjects);
-        }
     }
 }
