48619ab4285d802747d9883c447fdb8244e4e80b
AntoinouM <92710938+AntoinouM@users.noreply.github.com>
2023-06-15 11:16:00 +0200

    add: smoke effect on bump

--- Astroboy/Assets/Scripts/Enemies/StompCheck.cs
+++ Astroboy/Assets/Scripts/Enemies/StompCheck.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 
 public class StompCheck : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem explosionPS;
+    private ParticleSystem.MainModule _explosionPSMain;
+
+    private void Awake()
+    {
+        explosionPS.Stop();
+    }
+
+    private void Start()
+    {
+        _explosionPSMain = explosionPS.main;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<EnemyHeadStompCheck>())
         {
+            explosionPS.Play();
             Destroy(this.transform.parent.gameObject, 0.1f);
         }
     }
