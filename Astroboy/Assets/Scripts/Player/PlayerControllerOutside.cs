0cec4c0dd9eb284ad914122560bc38fac5a2f97a
Averigines <pitterlepatrick@gmail.com>
2023-06-21 01:21:59 +0200

    GameOver conditions

--- Astroboy/Assets/Scripts/Player/PlayerControllerOutside.cs
+++ Astroboy/Assets/Scripts/Player/PlayerControllerOutside.cs
@@ -122,5 +122,6 @@ public class PlayerControllerOutside : MonoBehaviour
     private void UseOxygen()
     {
         if (DataStorage.instance.CurrOxygen > 0) DataStorage.instance.LoseOxygen();
+        else SceneSwitcher.instance.LoadScene("GameOver");
     }
 }
