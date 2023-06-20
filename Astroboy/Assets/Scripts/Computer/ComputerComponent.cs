1e78c9e0ae0c784445d0803f2f9c8c5a6922688a
AntoinouM <92710938+AntoinouM@users.noreply.github.com>
2023-06-20 23:11:43 +0200

    add: computer menu

--- Astroboy/Assets/Scripts/Computer/ComputerComponent.cs
+++ Astroboy/Assets/Scripts/Computer/ComputerComponent.cs
@@ -1,12 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using TMPro.Examples;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
+
+[Serializable] public struct Buttons
+{
+    public Button btnProgress;
+    public Button btnLogs;
+    public Button btnBackProgress;
+    public Button btnBackLogs;
+    //public Button logsDown;
+    //public Button logsUp;
+}
+
+[Serializable]
+public struct DynamicFields
+{
+    public Image progressBar;
+    //public Text gemCollected;
+    //public Text crystalCollected;
+    public TextMeshProUGUI progressPercentage;
+}
 
 public class ComputerComponent : MonoBehaviour
 {
-    [SerializeField] private Button screenOnBtn;
+    [SerializeField] private Buttons buttons;
     [SerializeField] private Canvas[] canvas;
+    [SerializeField] private DynamicFields dynamicFields;
 
     private void SwitchCanvas(Canvas canvasOn)
     {
@@ -19,7 +43,10 @@ public class ComputerComponent : MonoBehaviour
     void Start()
     {
         SwitchCanvas(canvas[0]);
-        screenOnBtn.onClick.AddListener(delegate { SwitchCanvas(canvas[1]); });
+        buttons.btnProgress.onClick.AddListener(delegate { SwitchCanvas(canvas[1]); });
+        buttons.btnLogs.onClick.AddListener(delegate { SwitchCanvas(canvas[2]); });
+        buttons.btnBackProgress.onClick.AddListener(delegate { SwitchCanvas(canvas[0]); });
+        buttons.btnBackLogs.onClick.AddListener(delegate { SwitchCanvas(canvas[0]); });
     }
     
     
