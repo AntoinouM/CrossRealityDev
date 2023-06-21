1438682ddbfff4ba93999b4506b014f5aa0eec46
alonishay <aloni.daniel@gmail.com>
2023-06-21 13:27:41 +0200

    Audio Commit for merge

---
+++ Astroboy/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,55 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_COMPUTER = 1984956505U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace COMPUTER
+        {
+            static const AkUniqueID GROUP = 2376682602U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BOOT = 1761500993U;
+                static const AkUniqueID CANCEL = 2760337059U;
+                static const AkUniqueID CLICK = 1584507803U;
+                static const AkUniqueID TURN_OFF = 188147178U;
+            } // namespace SWITCH
+        } // namespace COMPUTER
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
